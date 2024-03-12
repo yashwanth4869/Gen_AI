b62b7cc0c69387c8bd4924afd455e87101f4de9e
yashwanth <yashwanthpapishetty7@gmail.com>
2024-03-12 15:14:23 +0530

    added backend

--- src/routes/query_routes.py
+++ src/routes/query_routes.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi.responses import Response
 from sqlalchemy.orm import Session
 from src.controllers.query_controller import QueryController
 from src.config.database_initializer import get_db
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
+
+@router.post('/query/{user_id}')
+async def generate_answer(user_id, request : Request, db : Session = Depends(get_db)):
+    return await QueryController(db).get_response(user_id, request)
