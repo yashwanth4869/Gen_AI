37e30aeb532d10f88920424b20e4c19a1ab09d5e
yashwanth <yashwanthpapishetty7@gmail.com>
2024-03-15 16:38:29 +0530

    added sql tool

--- src/services/gen_ai_service.py
+++ src/services/gen_ai_service.py
@@ -15,13 +15,18 @@ from sqlalchemy import MetaData
 from langchain.sql_database import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from src.config.database import engine
-
+from langchain import PromptTemplate
+from langchain_community.agent_toolkits import create_sql_agent
+from src.config.database_initializer import get_db
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from src.services.sql_tool import SQLCustomTool
 
 
 from dotenv import load_dotenv
 import os
 class GenAiService:
     def __init__(self, db: Session):
+
         self.user_dao = UserDAO(db)
 
     async def generate_response(self, user_query, user_id):
@@ -40,12 +45,23 @@ class GenAiService:
             template="{query}"
         )
 
+        prompt = hub.pull("hwchase17/react")
+
+        # prompt_template = """Use the following as it is:"{text}"DO NOT SUMMARIZE:"""
+        # PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+
         llm_chain = LLMChain(llm=llm, prompt=prompt)
 
         llm_math = LLMMathChain(llm=llm)
 
         search = DuckDuckGoSearchRun()
 
+        # db = SQLDatabase(engine)
+        # db = get_db()
+        # sql_chain = SQLDatabaseChain(llm = llm, database=db, verbose=True)
+        # sql_chain.limit = None
+
+        
         tools=[
             Tool(
                 name='Language Model',
@@ -63,42 +79,75 @@ class GenAiService:
                 description="useful for when you need to answer questions about current events",
             )
         ]
-        db = SQLDatabase(engine)
-        sql_chain = SQLDatabaseChain(llm = llm, database=db, verbose=True)
 
+        # tools.append
+        # db = SQLDatabase(engine)
+        # sql_chain = SQLDatabaseChain(llm = llm, database=db, verbose=True)
+        
         metadat_obj = MetaData()
 
-        sql_tool = Tool(
-        name = 'RealEstate DB',
-        func = sql_chain.run,
-        description = "Useful when you need to answer the question about the RealEstate and its prices"
-        )
-
+        # sql_tool = Tool(
+        #     name = 'RealEstate DB',
+        #     func = sql_chain.run,
+        #     description = "Useful when you need to answer the question about the RealEstate and its prices"
+        #     # top_k = 1000
+        #     )
+        sql_tool = SQLCustomTool()
         tools.append(sql_tool)
+        tools_list = list(tools)
         # Load the "arxiv" tool
+        
         arxiv_tool = load_tools(["arxiv"])
-
+        # tools.extend(sql_tool)
         # Add the loaded tool to your existing list
         tools.extend(arxiv_tool)
 
 
         prompt = hub.pull("hwchase17/react")
-
+        tools = sql_tool
         memory = ConversationBufferMemory(memory_key="chat_history")
-
         previous_chats = await self.user_dao.fetch_previous_chat(user_id)
         for chat in previous_chats:
             memory.chat_memory.add_user_message(chat['question'])
             memory.chat_memory.add_ai_message(chat['answer'])
+
+        # agent = create_sql_agent(   
+        # llm=llm,
+        # toolkit=SQLDatabaseToolkit(db=db, llm=llm),
+        # verbose=True,
+        # agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        # top_k=1000
+        # )
+
+        # PREFIX = """Answer the following questions as best you can. You have access to the following tools:
+        #             sql_tool, search tool, calculator tool, LLM tool, arxiv tool related to research papers"""
+        # FORMAT_INSTRUCTIONS = """
+        # Use the following format:
+
+
+        # Question: the input question you must answer
+        # Thought: you should always think about what to do
+        # Action: the action to take, should be one of [{tool_names}]
+        # Action Input: the input to the action
+        # Observation: the result of the action
+        # ... (this Thought/Action/Action Input/Observation can repeat N times)
+        # Thought: I now know the final answer
+        # Final Answer: the final answer to the original input question"""
+        # SUFFIX = """Begin!"""
+
         conversational_agent = initialize_agent(
             agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
-            tools=tools,
+            tools=tools_list,
             llm=llm,
             verbose=True,
-            max_iterations=3,
+            max_iterations=5,
             memory=memory,
-        )
-        output=conversational_agent.run(input=user_query)
+            handle_parsing_errors=True,
+            )
+
+
+        output = conversational_agent.run(input=user_query)
 
         return output
-       
+
+
---
+++ src/services/sql_tool.py
@@ -0,0 +1,72 @@
+from langchain.tools import BaseTool,StructuredTool, tool
+from langchain.agents.agent_types import AgentType
+from langchain.sql_database import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from src.config.database import engine
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+
+class SQLCustomTool(BaseTool):
+    name = "SQL querying tool"
+    description = '''
+                    **I want sql query to run everytime whenever data related to properties or data related to houses or data related to real estate is needed.
+                    **Use this tool when you need to answer the questions related to RealEstate Data or RelaEstate Database. The user can also query using words like "house, property, city, state, size, other terminologies related to houses or properties, etc".  "Expects a question from the user and queries that question and return the answer according to the data present" "To use this tool you must provide the direct question from user
+                    **The table consists of fields- 
+                    *id, 
+                    *status (for_sale or sold out), 
+                    *Bed (represents numbers of bedrooms in house or property),
+                    *Bath (reprsents number of bathrooms in the property),
+                    *AcreLot,
+                    *City,
+                    *State,
+                    *ZipCode,
+                    *HouseSize (represnts the size of the house),
+                    *Price (represnts cost of the house/property) "'''
+
+    def _run(self,query:str):
+        load_dotenv()
+        api_key=os.getenv("OPENAI_API_KEY")
+
+        llm = OpenAI(
+            openai_api_key=api_key,
+            temperature=0
+        )
+        db = SQLDatabase(engine)
+        agent = create_sql_agent(   
+            llm=llm,
+            toolkit=SQLDatabaseToolkit(db=db, llm=llm),
+            verbose=True,
+            agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            top_k=1000
+            )
+        return agent.run(query)
+    
+    def _arun(self,query:str):
+        raise NotImplementedError("This tool does not support async :")
+
+
+# @tool
+# ""
+# def sql_custom_agent(query:str):
+
+#     load_dotenv()
+#     api_key=os.getenv("OPENAI_API_KEY")
+
+#     llm = OpenAI(
+#         openai_api_key=api_key,
+#         temperature=0
+#     )
+#     db = SQLDatabase(engine)
+#     agent = create_sql_agent(   
+#         llm=llm,
+#         toolkit=SQLDatabaseToolkit(db=db, llm=llm),
+#         verbose=True,
+#         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+#         top_k=1000
+#         )
+#     return agent.run(query)
