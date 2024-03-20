c465279df0ba39d3934bc132242e7ce377d2cfa1
Ritesh-ch7 <riteshch098@gmail.com>
2024-03-20 18:31:46 +0530

    (feat) Added CSV querying feature

--- src/services/gen_ai_service.py
+++ src/services/gen_ai_service.py
@@ -3,13 +3,16 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import LLMMathChain
 from langchain.tools import DuckDuckGoSearchRun
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain import hub
 from langchain.agents import initialize_agent, Tool, load_tools
 from langchain.memory import ConversationBufferMemory
 from langchain.agents.agent_types import AgentType
 from src.dao.user_dao import UserDAO
 from langchain import PromptTemplate
-from src.services.sql_tool import SQLCustomTool
+from src.services.sql_chain_tool import SQLCustomTool
+from src.services.csv_tool import CSVCustomTool
 from langchain_community.chat_message_histories.upstash_redis import UpstashRedisChatMessageHistory
 from dotenv import load_dotenv
 import os
@@ -69,6 +72,10 @@ class GenAiService:
 
         sql_tool = SQLCustomTool()
         tools.append(sql_tool)
+
+        csv_tool=CSVCustomTool()
+        tools.append(csv_tool)
+
         # Load the "arxiv" tool
         arxiv_tool = load_tools(["arxiv"])
 
@@ -78,7 +85,7 @@ class GenAiService:
 
         prompt = hub.pull("hwchase17/openai-functions-agent")
 
-        memory = ConversationBufferMemory(memory_key="chat_history")
+        # memory = ConversationBufferMemory(memory_key="chat_history")
 
 
         conversational_agent = initialize_agent(
