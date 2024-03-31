7618a9be8afa35c55c9b3afcb453b66e01b45091
Manoo07 <manoharboinapalli1196@gmail.com>
2024-03-31 13:56:26 +0530

    Stop tracking myenv directory

--- myenv/lib/python3.11/site-packages/langchain_experimental/recommenders/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Amazon Personalize primitives."""
-from langchain_experimental.recommenders.amazon_personalize import AmazonPersonalize
-from langchain_experimental.recommenders.amazon_personalize_chain import (
-    AmazonPersonalizeChain,
-)
-
-__all__ = ["AmazonPersonalize", "AmazonPersonalizeChain"]
